bac98f7ce6e8ca230c3a1851855a1b728688226f
Yamikani Chinamale <ychinamale@gmail.com>
2026-03-15 03:47:08 +0200

    feat(panics): apply apiKeyGuard(PANIC_SOURCE) preHandler [GREEN]

--- api/src/routes/panics.ts
+++ api/src/routes/panics.ts
@@ -1,7 +1,13 @@
 import type { FastifyInstance } from 'fastify'
 
+import { apiKeyGuard } from '../hooks/apiKeyGuard.js'
+
 export function panicRoutes(fastify: FastifyInstance) {
-  fastify.post('/api/v1/panics', async (_request, reply) => {
-    return reply.code(501).send()
-  })
+  fastify.post(
+    '/api/v1/panics',
+    { preHandler: apiKeyGuard('PANIC_SOURCE') },
+    async (_request, reply) => {
+      return reply.code(501).send()
+    },
+  )
 }
